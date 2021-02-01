037fa810189804fd8622786a88b1aae143e6ebee
ashikmahmud <ashikmamud187@gmail.com>
2021-02-02 01:05:30 +0600

    I have finished in my task making fiverr home page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+
+    // Header Fixed Menu JS Code 
+    $(window).scroll(function(){
+        var scrolWidth = $(this).scrollTop();
+        if(scrolWidth>70){
+            $(".navbar-fixed-site").css({opacity: 1});
+            
+        }else{
+            $(".navbar-fixed-site").css({opacity: 0});
+        }
+    })
+    $(window).scroll(function(){
+        var scrolWidth1 = $(this).scrollTop();
+        if(scrolWidth1>200){
+            $(".category").addClass("showCategory");
+        }else{
+            $(".category").removeClass("showCategory");
+        }
+    })
+
     // Home Slider JS Code Start
        setInterval(function(){
             if($(".slide-items-5").hasClass("opacity")){
@@ -50,6 +70,69 @@ $(document).ready(function(){
         ]
 
     })
+     // Initialization Slick Slider 
+
+
+     $(".test-content").slick({
+        infinite: true,
+        autoplay: false,
+        slidesToShow: 1,
+        slidesToScroll:1,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+            }
+        ]
+
+    })
+     // Initialization Get Freelancer Slick Slider 
+
+
+     $(".get-freelancer").slick({
+        infinite: true,
+        autoplay: false,
+        slidesToShow: 4,
+        slidesToScroll:4,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+            }
+        ]
+
+    })
     $(".slick-prev").html("<i class='h5 pt-2 text-muted fas fa-angle-left'></i>");
     $(".slick-next").html("<i class='h5 pt-2 text-muted fas fa-angle-right'></i>");
+
+
+
 })
